597485723880a485c78a2ac08d38e622e21decb5
Glenn Rempe <glenn@rempe.us>
2009-07-07 13:40:32 -0700

    Added array of paths to cookbook_path in solo.rb.  Submoduled opscode cookbooks.

--- config/solo.rb
+++ config/solo.rb
@@ -1,4 +1,8 @@
-cookbook_path File.join(File.dirname(__FILE__), '..', "cookbooks")
+cookbook_path [ 
+                File.join(File.dirname(__FILE__), '..', "site-cookbooks"),
+                File.join(File.dirname(__FILE__), '..', "cookbooks")
+              ]
+
 log_level :debug
 file_store_path File.join(File.dirname(__FILE__), '..')
 file_cache_path File.join(File.dirname(__FILE__), '..')
