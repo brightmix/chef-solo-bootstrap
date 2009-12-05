9f2777b332a58375b23917c38ea9f9c48db66bfa
Dusty Davidson <dusty@dusty-davidsons-macbook-pro.local>
2009-12-05 16:01:02 -0600

    added more stuff

--- config/solo.rb
+++ config/solo.rb
@@ -2,7 +2,7 @@ cookbook_path [
                 File.join(File.dirname(__FILE__), '..', "site-cookbooks"),
                 File.join(File.dirname(__FILE__), '..', "cookbooks")
               ]
-
+role_path File.join(File.dirname(__FILE__), '..', "roles")
 log_level :debug
 file_store_path File.join(File.dirname(__FILE__), '..')
 file_cache_path File.join(File.dirname(__FILE__), '..')
