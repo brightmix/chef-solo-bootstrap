c26f75d0590a8b7e772c02027395a039fc538f05
Dusty Davidson <dusty@dusty-davidsons-macbook-pro.local>
2009-12-05 16:14:33 -0600

    added more stuff

--- config/solo.rb
+++ config/solo.rb
@@ -2,9 +2,9 @@ cookbook_path [
                 File.join(File.dirname(__FILE__), '..', "site-cookbooks"),
                 File.join(File.dirname(__FILE__), '..', "cookbooks")
               ]
-role_path File.join(File.dirname(__FILE__), '..', "roles")
 log_level :debug
 file_store_path File.join(File.dirname(__FILE__), '..')
 file_cache_path File.join(File.dirname(__FILE__), '..')
+role_path File.join(File.dirname(__FILE__), '..', "roles")
 Chef::Log::Formatter.show_time = false
 
